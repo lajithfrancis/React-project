b0c557d50e85f1c08ad4bec6aa9a65d6fe5667d6
lajithfrancis <lajith.francis@perfomatix.com>
2023-04-12 20:22:07 +0530

    updated TextField properties

--- src/components/tasks/taskList.js
+++ src/components/tasks/taskList.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import './styles.css'
 import { Button, Checkbox, TextField } from '@mui/material';
-export default function TaskList({tasks, onChangeTask, onDeleteTask}) {
+export default function TaskList({ tasks, onChangeTask, onDeleteTask }) {
   return (
     <ul>
       {tasks.map((task) => (
@@ -13,20 +13,20 @@ export default function TaskList({tasks, onChangeTask, onDeleteTask}) {
   );
 }
 
-function Task({task, onChange, onDelete}) {
+function Task({ task, onChange, onDelete }) {
   const [isEditing, setIsEditing] = useState(false);
   let taskContent;
   if (isEditing) {
     taskContent = (
       <>
-      <TextField id="outlined-basic" label="Task" variant="outlined" placeholder='Edit task'
-      value={task.text}
-      onChange={(e) => {
-        onChange({
-          ...task,
-          text: e.target.value,
-        });
-      }}
+        <TextField id="outlined-basic" label="Task" variant="outlined" placeholder='Edit task'
+          value={task.text}
+          onChange={(e) => {
+            onChange({
+              ...task,
+              text: e.target.value,
+            });
+          }}
         />
         <Button variant="contained" color="success"
           onClick={() => setIsEditing(false)}
@@ -38,15 +38,18 @@ function Task({task, onChange, onDelete}) {
   } else {
     taskContent = (
       <>
-      <TextField id="outlined-basic" label="" variant="outlined" placeholder='Edit task'
-      value={task.text}
-      onChange={(e) => {
-        onChange({
-          ...task,
-          text: e.target.value,
-        });
-      }}
-      />
+        <TextField id="outlined-read-only-input" label="" variant="outlined" placeholder='Edit task'
+          InputProps={{
+            readOnly: true,
+          }}
+          value={task.text}
+          onChange={(e) => {
+            onChange({
+              ...task,
+              text: e.target.value,
+            });
+          }}
+        />
         <Button variant="contained" color="secondary"
           onClick={() => setIsEditing(true)}
         >
@@ -57,15 +60,15 @@ function Task({task, onChange, onDelete}) {
   }
   return (
     <label>
-      <Checkbox 
-      type="checkbox"
-      checked={task.done}
-      onChange={(e) => {
-        onChange({
-          ...task,
-          done: e.target.checked,
-        });
-      }}
+      <Checkbox
+        type="checkbox"
+        checked={task.done}
+        onChange={(e) => {
+          onChange({
+            ...task,
+            done: e.target.checked,
+          });
+        }}
       />
       {taskContent}
       <Button variant="contained" color="error"
