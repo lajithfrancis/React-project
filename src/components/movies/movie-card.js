dcfa90c66b9b724f24d447b75ba40bf13b179462
lajithfrancis <lajith.francis@perfomatix.com>
2023-04-13 20:04:45 +0530

    modal page with movie details

--- src/components/movies/movie-card.js
+++ src/components/movies/movie-card.js
@@ -4,11 +4,23 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
+import { useState } from 'react';
+import movieDetails from '../../data/movie.json';
+import ModalArea from './modal-details-page';
+
 
 export default function ActionAreaCard({ data }) {
+  const [open, setOpen] = useState(false);
+  const [movie, setMovie] = useState('')
+  const handleClickAction = () => {
+    setOpen(true);
+    setMovie(movieDetails)
+  }
+
   return (
     <Card sx={{ maxWidth: 345 }}>
-      <CardActionArea>
+      <CardActionArea onMouseDown={(e) => handleClickAction()}>
+        { open? <ModalArea data={movie} isOpen={open} setOpen={setOpen}/> : null}
         <CardMedia
           component="img"
           height="300"
