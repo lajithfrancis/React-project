18b20be7c9c46d562d6cb4504c4e0b31df2645a5
lajithfrancis <lajith.francis@perfomatix.com>
2023-04-13 20:36:15 +0530

    api integrated

--- src/components/movies/movie-card.js
+++ src/components/movies/movie-card.js
@@ -4,23 +4,38 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
-import { useState } from 'react';
-import movieDetails from '../../data/movie.json';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 import ModalArea from './modal-details-page';
 
 
 export default function ActionAreaCard({ data }) {
   const [open, setOpen] = useState(false);
   const [movie, setMovie] = useState('')
-  const handleClickAction = () => {
+  const [movieData, setMovieData] = useState([]);
+
+  const handleClickAction = (clickedMovie) => {
     setOpen(true);
-    setMovie(movieDetails)
+    setMovie(clickedMovie)
   }
+
+  useEffect(() => {
+    if (open && movie.imdbID) {
+      axios.get(`https://www.omdbapi.com/?apikey=e7570bb3&i=${movie.imdbID}`)
+      .then(response => {
+        setMovieData(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+    }
+  }, [open]);
 
   return (
     <Card sx={{ maxWidth: 345 }}>
-      <CardActionArea onMouseDown={(e) => handleClickAction()}>
-        { open? <ModalArea data={movie} isOpen={open} setOpen={setOpen}/> : null}
+      <CardActionArea onMouseDown={(e) => handleClickAction(data)}>
+        { open? <ModalArea data={movieData} isOpen={open} setOpen={setOpen}/> : null}
         <CardMedia
           component="img"
           height="300"
