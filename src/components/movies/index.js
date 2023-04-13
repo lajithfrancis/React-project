51a2885e3e27d28b1a467485033ec18bb0475451
LajithFrancis <lajith.francis@perfomatix.com>
2023-04-13 13:04:24 +0530

    movie card work in-progress

--- src/components/movies/index.js
+++ src/components/movies/index.js
@@ -1,7 +1,10 @@
+import ActionAreaCard from "./movie-card";
+
 export default function MoviePage() {
     return (
         <>
           <h1>Movie list</h1>
+          <ActionAreaCard/>
         </>
       );
 }
---
+++ src/components/movies/movie-card.js
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+
+export default function ActionAreaCard() {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image="/static/images/cards/image.jpg"
+          alt="movie image"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Movie title
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+          A film – also called a movie, motion picture, moving picture, picture, photoplay or (slang) flick – is a work of visual art that simulates experiences and otherwise communicates ideas, stories, perceptions, feelings, beauty, or atmosphere through the use of moving images.
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
