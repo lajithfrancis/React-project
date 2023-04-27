1aa4918b54daa61de32adf2cebf1f2b2e9d8b494
LajithFrancis <lajith.francis@perfomatix.com>
2023-04-27 21:17:05 +0530

    cards view - WIP

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Tasks from './components/tasks'
 import Section from './components/context/section'
 import { Route, Routes } from 'react-router-dom'
 import MoviePage from './components/movies'
+import Cards from './components/cards'
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Tasks />} />
         <Route path="/movies" element={<MoviePage />} />
+        <Route path="/cards" element={<Cards />} />
       </Routes>
       <Section level={10}>
         </Section>
---
+++ src/components/cards/index.js
@@ -0,0 +1,37 @@
+import './styles.css'
+
+
+export default function Cards() {
+    return (
+      <div className='row'>
+        <div className='column'>
+          <div className='card'>
+            <h3>Card 1</h3>
+            <p>Some text</p>
+            <p>Some text</p>
+          </div>
+        </div>
+        <div className='column'>
+          <div className='card'>
+            <h3>Card 1</h3>
+            <p>Some text</p>
+            <p>Some text</p>
+          </div>
+        </div>
+        <div className='column'>
+          <div className='card'>
+            <h3>Card 1</h3>
+            <p>Some text</p>
+            <p>Some text</p>
+          </div>
+        </div>
+        <div className='column'>
+          <div className='card'>
+            <h3>Card 1</h3>
+            <p>Some text</p>
+            <p>Some text</p>
+          </div>
+        </div>
+      </div>
+    );
+}
