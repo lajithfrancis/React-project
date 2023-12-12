cd56c1b07e22acb39b2b5e7a813e25e8b50d2de6
LajithFrancis <lajithf@gmail.com>
2023-12-12 20:44:30 +0530

    Add sidebar and navbar components.

--- src/components/tasks/index.js
+++ src/components/tasks/index.js
@@ -4,11 +4,13 @@ import TaskList from './taskList.js';
 import tasksReducer from './taskReducer.js';
 import './styles.css'
 import { useNavigate } from "react-router-dom";
+import SideBar from './sidebar.js';
+import NavBar from './navbar.js';
 
 export default function TaskApp() {
   const navigate = useNavigate();
 
-  const [tasks, dispatch] = useReducer(tasksReducer, initialTasks)
+  const [tasks, dispatch] = useReducer(tasksReducer, initialTasks);
 
   function handleAddTask(text) {
     dispatch({
@@ -22,32 +24,37 @@ export default function TaskApp() {
   function handleChangeTask(task) {
     dispatch({
       type: 'changed',
-      task
+      task,
     });
   }
 
   function handleDeleteTask(taskId) {
     dispatch({
       type: 'deleted',
-      id: taskId
+      id: taskId,
     });
   }
 
-
   const handleClick = () => {
-    navigate("/movies");
-  }
+    navigate('/movies');
+  };
 
   return (
     <>
-      <div className='container' style={{ position: 'relative' }}>
-        <h1>TODO list</h1>
-        <AddTask onAddTask={handleAddTask} />
-        <TaskList
-          tasks={tasks}
-          onChangeTask={handleChangeTask}
-          onDeleteTask={handleDeleteTask}
-        />
+      <div style={{ display: 'flex' }}>
+        {SideBar()}
+        <div style={{ display: 'grid', gap: '5rem', width: '100%' }}>
+          {NavBar()}
+          <div className='container' style={{ position: 'relative' }}>
+            {/* <h1>TODO list</h1> */}
+            <AddTask onAddTask={handleAddTask} />
+            <TaskList
+              tasks={tasks}
+              onChangeTask={handleChangeTask}
+              onDeleteTask={handleDeleteTask}
+            />
+          </div>
+        </div>
       </div>
     </>
   );
