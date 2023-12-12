8a417c8252dffe39b61dc478250cd312b46abbaf
LajithFrancis <lajithf@gmail.com>
2023-12-12 10:59:16 +0530

    Update routing in the Cards component

--- src/components/cards/index.js
+++ src/components/cards/index.js
@@ -1,37 +1,55 @@
 import './styles.css'
-
+import { useNavigate } from 'react-router-dom';
 
 export default function Cards() {
-    return (
-      <div className='row'>
-        <div className='column'>
-          <div className='card'>
-            <h3>Card 1</h3>
-            <p>Some text</p>
-            <p>Some text</p>
-          </div>
-        </div>
-        <div className='column'>
-          <div className='card'>
-            <h3>Card 1</h3>
-            <p>Some text</p>
-            <p>Some text</p>
-          </div>
-        </div>
-        <div className='column'>
-          <div className='card'>
-            <h3>Card 1</h3>
-            <p>Some text</p>
-            <p>Some text</p>
-          </div>
-        </div>
-        <div className='column'>
-          <div className='card'>
-            <h3>Card 1</h3>
-            <p>Some text</p>
-            <p>Some text</p>
-          </div>
+  const navigate = useNavigate();
+  const handleClick = (page) => {
+    navigate(`/${page}`);
+  };
+  return (
+    <div className='row'>
+      <div className='column'>
+        <div className='card'>
+          <h3>ToDo App</h3>
+          <p>Tasks</p>
+          <p>Happy tasking!</p>
+          <button
+            onClick={() => {
+              handleClick('tasks');
+            }}
+          >
+            Go
+          </button>
         </div>
       </div>
-    );
+      <div className='column'>
+        <div className='card'>
+          <h3>Movies App</h3>
+          <p>Happy movie</p>
+          <p>Enjoy!</p>
+          <button
+            onClick={() => {
+              handleClick('movies');
+            }}
+          >
+            Go
+          </button>
+        </div>
+      </div>
+      <div className='column'>
+        <div className='card'>
+          <h3>Coming soon!</h3>
+          <p>Some text</p>
+          <p>Some text</p>
+        </div>
+      </div>
+      <div className='column'>
+        <div className='card'>
+          <h3>Coming soon!</h3>
+          <p>Some text</p>
+          <p>Some text</p>
+        </div>
+      </div>
+    </div>
+  );
 }
