11b3c71f35c141fe1b6f01a7c0e8a3af7facaa53
Lajith <lajith.francis@perfomatix.com>
2023-12-12 14:29:22 +0530

    Update homepage layout and add Bootstrap

--- src/components/cards/index.js
+++ src/components/cards/index.js
@@ -4,55 +4,58 @@ import { useNavigate } from 'react-router-dom';
 export default function Cards() {
   const navigate = useNavigate();
   const handleClick = (page) => {
-    navigate(`/${page}`);
+      navigate(`/${page}`);
   };
   return (
-    <div
-      className='row'
-      style={{ display: 'flex', justifyContent: 'space-evenly' }}
-    >
-      <div className='column'>
-        <div className='card'>
-          <h3>ToDo App</h3>
-          <p>Tasks</p>
-          <p>Happy tasking!</p>
-          <button
-            onClick={() => {
-              handleClick('tasks');
-            }}
-          >
-            Go
-          </button>
-        </div>
-      </div>
-      <div className='column'>
-        <div className='card'>
-          <h3>Movies App</h3>
-          <p>Happy movie</p>
-          <p>Enjoy!</p>
-          <button
-            onClick={() => {
-              handleClick('movies');
-            }}
-          >
-            Go
-          </button>
-        </div>
-      </div>
-      <div className='column'>
-        <div className='card'>
-          <h3>Coming soon!</h3>
-          <p>Some text</p>
-          <p>Some text</p>
-        </div>
-      </div>
-      <div className='column'>
-        <div className='card'>
-          <h3>Coming soon!</h3>
-          <p>Some text</p>
-          <p>Some text</p>
-        </div>
-      </div>
-    </div>
+      <>
+          <div class="container px-4 py-5" id="featured-3">
+              <h2 class="pb-2 border-bottom">My Projects</h2>
+              <div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
+                  <div class="feature col">
+                      <h3 class="fs-2 text-body-emphasis">ToDo</h3>
+                      <p>Daily Todo Application for everyone!</p>
+                      <button
+                          type="button"
+                          className="btn btn-dark"
+                          style={{ width: '50%' }}
+                          onClick={() => {
+                              handleClick('tasks');
+                          }}
+                      >
+                          Go
+                      </button>
+                  </div>
+                  <div class="feature col">
+                      <h3 class="fs-2 text-body-emphasis">MOViE</h3>
+                      <p>
+                          Paragraph of text beneath the heading to explain the
+                          heading. We'll add onto it with another sentence and
+                          probably just keep going until we run out of words.
+                      </p>
+                      <button
+                          type="button"
+                          className="btn btn-dark"
+                          style={{ width: '50%' }}
+                          onClick={() => {
+                              handleClick('movies');
+                          }}
+                      >
+                          Go
+                      </button>
+                  </div>
+                  <div class="feature col">
+                      <h3 class="fs-2 text-body-emphasis">Coming soon!</h3>
+                      <p>
+                          Paragraph of text beneath the heading to explain the
+                          heading. We'll add onto it with another sentence and
+                          probably just keep going until we run out of words.
+                      </p>
+                      <a href="#" class="icon-link">
+                          Call to action
+                      </a>
+                  </div>
+              </div>
+          </div>
+      </>
   );
 }
