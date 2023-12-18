44aaa117945a9d964f45d00767c038fa4504f6b2
LajithFrancis <lajithf@gmail.com>
2023-12-18 12:03:53 +0530

    Added a new route for the Youtube page.

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import Tasks from './components/tasks'
 import Section from './components/context/section'
 import { Route, Routes } from 'react-router-dom'
 import MoviePage from './components/movies'
+import YoutubePage from './components/youtube';
 
 export default function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Tasks />} />
-        <Route path="/movies" element={<MoviePage />} />
+        <Route path='/' element={<Tasks />} />
+        <Route path='/movies' element={<MoviePage />} />
+        <Route path='/youtube' element={<YoutubePage />} />
       </Routes>
-      <Section level={10}>
-        </Section>
+      <Section level={10}></Section>
     </>
-
-  )
+  );
 }
---
+++ src/components/youtube/index.js
@@ -0,0 +1,7 @@
+export default function YoutubePage() {
+  return (
+    <>
+      <div>Youtube</div>
+    </>
+  );
+}
