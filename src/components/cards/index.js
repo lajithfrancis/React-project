a062cd49345dd7207e0169d6fc1221fab2b7eaef
LajithFrancis <lajithf@gmail.com>
2023-12-18 12:32:04 +0530

    Added a Youtube page with a navigation bar.

--- src/components/cards/index.js
+++ src/components/cards/index.js
@@ -44,15 +44,18 @@ export default function Cards() {
             </button>
           </div>
           <div className='feature col movie-card'>
-            <h3 className='fs-2 text-body-emphasis'>Coming soon!</h3>
-            <p>
-              Paragraph of text beneath the heading to explain the heading.
-              We'll add onto it with another sentence and probably just keep
-              going until we run out of words.
-            </p>
-            <a href='#' className='icon-link'>
-              Call to action
-            </a>
+            <h3 className='fs-2 text-body-emphasis'>Youtube</h3>
+            <p>Youtube UI experiment</p>
+            <button
+              type='button'
+              className='btn btn-dark'
+              style={{ width: '50%' }}
+              onClick={() => {
+                handleClick('youtube');
+              }}
+            >
+              Go
+            </button>
           </div>
         </div>
       </div>
--- src/components/youtube/index.js
+++ src/components/youtube/index.js
@@ -1,7 +1,13 @@
+import './index.css';
+
 export default function YoutubePage() {
   return (
     <>
-      <div>Youtube</div>
+      <nav className='nav'>
+        <div className='item1'>Youtube logo</div>
+        <div className='item2'>search</div>
+        <div className='item1'>account</div>
+      </nav>
     </>
   );
 }
