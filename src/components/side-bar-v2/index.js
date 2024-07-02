7b465f88c6d34e570d4a89e083eb16200837997f
LajithFrancis <lajithf@gmail.com>
2024-07-02 16:21:19 +0530

    "Updated SideBarV2 component with new styles, icons, and functionality"

--- src/components/side-bar-v2/index.js
+++ src/components/side-bar-v2/index.js
@@ -1,67 +1,93 @@
 import Dashboard from '@mui/icons-material/Dashboard';
 import Category from '@mui/icons-material/Category';
+import Profile from '@mui/icons-material/Pages';
+import Settings from '@mui/icons-material/Settings';
+import Menu from '@mui/icons-material/Menu';
 import ArrowDropDown from '@mui/icons-material/ArrowDropDown';
 
 import './index.css';
+import { Button } from '@mui/material';
+import { useState } from 'react';
 
 const SideBarV2 = () => {
+  const [isOpen, setIsOpen] = useState(true);
   return (
-    <div className='sidebar'>
-      <div className='logo-details'>
-        <span className='logo-name'>Lajith Coding</span>
+    <div>
+      <div className={`sidebar ${!isOpen ? 'sidebar-close' : ''}`}>
+        <div className='logo-details'>
+          <span className='logo-name'>Lajith Coding</span>
+        </div>
+        {/* <Button onClick={() => setIsOpen(!isOpen)}>toggle</Button> */}
+
+        <ul className='nav-links'>
+          <li className=''>
+            <a href='#'>
+              <i className='bx bx-grid-alt'>
+                <Dashboard />
+              </i>
+              <span className='link_name'>Dashboard</span>
+            </a>
+          </li>
+          <li className=''>
+            <div className='iocn-link'>
+              <a href='#'>
+                <i className='bx bx-collection'>
+                  <Category />
+                </i>
+                <span className='link_name'>Categories</span>
+              </a>
+              <i className='bx bx-collection'>
+                <ArrowDropDown />
+              </i>
+            </div>
+            <ul className='sub-menu'>
+              <li>
+                <a href='#'>My tasks</a>
+              </li>
+              <li>
+                <a href='#'>Completed tasks</a>
+              </li>
+            </ul>
+          </li>
+          <li className=''>
+            <div className='iocn-link'>
+              <a href='#'>
+                <i className='bx bx-collection'>
+                  <Profile />
+                </i>
+                <span className='link_name'>Profile</span>
+              </a>
+              <i className='bx bx-collection'>
+                <ArrowDropDown />
+              </i>
+            </div>
+            <ul className='sub-menu'>
+              <li>
+                <a href='#'>First</a>
+              </li>
+              <li>
+                <a href='#'>Second</a>
+              </li>
+            </ul>
+          </li>
+          <li className=''>
+            <a href='#'>
+              <i className='bx bx-grid-alt'>
+                <Settings />
+              </i>
+              <span className='link_name'>Settings</span>
+            </a>
+          </li>
+        </ul>
       </div>
-      <ul className='nav-links'>
-        <li className=''>
-          <a href='#'>
-            <i className='bx bx-grid-alt'>
-              <Dashboard />
-            </i>
-            <span className='link_name'>Dashboard</span>
-          </a>
-        </li>
-        <li className=''>
-          <div className='iocn-link'>
-            <a href='#'>
-              <i className='bx bx-collection'>
-                <Category />
-              </i>
-              <span className='link_name'>Categories</span>
-            </a>
-            <i className='bx bx-collection'>
-              <ArrowDropDown />
-            </i>
-          </div>
-          <ul className='sub-menu'>
-            <li>
-              <a href='#'>My tasks</a>
-            </li>
-            <li>
-              <a href='#'>Completed tasks</a>
-            </li>
-          </ul>
-        </li>
-        <li className=''>
-          <div className='iocn-link'>
-            <a href='#'>
-              <i className='bx bx-collection'>
-                <Category />
-              </i>
-              <span className='link_name'>Categories</span>
-            </a>
-            <i className='bx bx-collection'>
-              <ArrowDropDown />
-            </i>
-          </div>
-          <ul className='sub-menu'>
-            <li>
-              <a href='#'>First</a>
-            </li>
-            <li>
-              <a href='#'>Second</a>
-            </li>
-          </ul>
-        </li>
-      </ul>
+      <section className='home-section'>
+        <div className='home-content'>
+          <i className='bx bx-menu' onClick={() => setIsOpen(!isOpen)}>
+            <Menu />
+          </i>
+          <span className='text'>Drop down sidebar</span>
+        </div>
+      </section>
     </div>
   );
 };
