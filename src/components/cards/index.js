231af5a2b88eba258e0b08b3041c5575b186637a
LajithFrancis <lajithf@gmail.com>
2024-07-09 21:30:29 +0530

    "Added Material-UI components and react-slick carousel to Cards component, updated styles.css to include slick-carousel styles"

--- src/components/cards/index.js
+++ src/components/cards/index.js
@@ -1,7 +1,69 @@
+import {
+  Card as MuiCard,
+  CardContent,
+  Typography,
+  Box,
+  Divider,
+} from '@mui/material';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
 import Card from './card';
+
 import './styles.css';
+import { Star } from '@mui/icons-material';
+
+const cardsData = [
+  {
+    name: 'Canvas',
+    description: `Application for Content Creators for monetize their work.`,
+    tech: 'Node. JS - Express, MongoDB, Next JS, NFT, Opensea, Web3.js, Metamask',
+  },
+  {
+    name: 'Emo Myloh',
+    description: `The deliverable is an eBuddy mobile application that helps users fulfill their goals and build mental resilience`,
+    tech: 'Node. JS - NestJS, Postgres',
+  },
+  {
+    name: 'Nuper',
+    description: `A Taxi Booking Application similar to Uber`,
+    tech: 'Node. JS - Express, MongoDB.',
+  },
+  {
+    name: 'Data Automation Tool',
+    description: `Application for Data automation to reduce the manual effort by 90%.`,
+    tech: 'Node JS- loopback and Postgres',
+  },
+];
 
 export default function Cards() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
   const projects = [
     {
       title: 'ToDo',
@@ -31,16 +93,60 @@ export default function Cards() {
           ))}
           <div className='feature col movie-card'>
             <h3 className='fs-2 text-body-emphasis'>Coming soon!</h3>
-            <p>
-              Paragraph of text beneath the heading to explain the heading.
-              We'll add onto it with another sentence and probably just keep
-              going until we run out of words.
-            </p>
+            <p>On the way...</p>
             <a href='#' className='icon-link'>
               Call to action
             </a>
           </div>
         </div>
+      </div>
+      <div className='container'>
+        <h2
+          className='pb-2 border-bottom'
+          style={{
+            bottom: '32px',
+            position: 'relative',
+          }}
+        >
+          My Key Projects
+        </h2>
+        <Slider {...settings}>
+          {cardsData.map((card, index) => (
+            <MuiCard
+              key={index}
+              sx={{
+                margin: '0 10px',
+                boxShadow: 3,
+                maxWidth: '400px',
+                height: '200px',
+              }}
+            >
+              <CardContent sx={{ padding: '20px' }}>
+                <Box display='flex' alignItems='center' mb={1}>
+                  <Star color='primary' sx={{ mr: 1 }} />
+                  <Typography
+                    variant='h5'
+                    component='div'
+                    sx={{ fontWeight: 'bold' }}
+                  >
+                    {card.name}
+                  </Typography>
+                </Box>
+                <Typography
+                  variant='body2'
+                  color='textSecondary'
+                  sx={{ marginBottom: '10px' }}
+                >
+                  {card.description}
+                </Typography>
+                <Divider sx={{ marginBottom: '10px' }} />
+                <Typography variant='body2' color='textPrimary'>
+                  <strong>Technologies:</strong> {card.tech}
+                </Typography>
+              </CardContent>
+            </MuiCard>
+          ))}
+        </Slider>
       </div>
     </>
   );
