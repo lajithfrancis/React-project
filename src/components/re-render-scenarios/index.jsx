2d3f2bd3d3e254f11ae315e745e4f0ef3c0fb381
Lajith Francis <502605@containerstore.com>
2024-09-04 11:35:41 +0530

    "Update Child component text and modify ReRenderScenarios component state and button text"

--- src/components/re-render-scenarios/index.jsx
+++ src/components/re-render-scenarios/index.jsx
@@ -3,18 +3,18 @@ import Child from "./child";
 
 export default function ReRenderScenarios() {
   const [toggle, setToggle] = useState(true);
-  const [info, setInfo] = useState('child')
+  const [info, setInfo] = useState()
   useEffect(() => {
     console.log(`Parent re-rendered`);
   }, [toggle]);
   const handleOnClick = () => {
     setToggle(!toggle);
-    setInfo(`'child': ${toggle}`)
+    setInfo(`${toggle}`)
   }
   return (
     <>
       <h1>Re-render scenarios</h1>
-      <button onClick={handleOnClick}>Re-render para</button>
+      <button onClick={handleOnClick}>Check</button>
       <Child name={info} />
     </>
   );
